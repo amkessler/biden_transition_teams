85a1ca5591d26ef8ce10a123e5f380f0f8604c7f
Kessler <ak113714@bna.com>
2020-11-23 15:52:52 -0500

    pulled out team lead designation

--- 02_analysis.R
+++ 02_analysis.R
@@ -5,4 +5,27 @@ library(writexl)
 #load saved data from step 01
 transition_data_scraped <- readRDS("processed_data/transition_data_scraped.rds")
 
-transition_data_scraped
+
+#some names have title after denoting they are the team lead
+#we'll capture that and remove title from name field
+agencyteams <- transition_data_scraped %>% 
+  mutate(
+    team_lead = if_else(str_detect(name, "Team Lead"), "Y", ""),
+    name = str_remove(name, ", Team Lead")
+  ) %>% 
+  select(agency, name, team_lead, everything())
+
+agencyteams
+
+
+#quick counts
+agencyteams %>% 
+  count(agency, sort = TRUE)
+
+agencyteams %>% 
+  count(most_recent_employment, sort = TRUE)
+
+agencyteams %>% 
+  count(source_of_funding, sort = TRUE)
+
+

fd075710f981cc90840b009f19a394f4528f862c
Kessler <ak113714@bna.com>
2020-11-24 12:44:38 -0500

    moved team lead to scraping script; flipped order so comparisons first

--- 03_analysis.R
+++ 03_analysis.R
@@ -3,50 +3,10 @@ library(janitor)
 library(writexl)
 
 
-#### AGENCY TEAMS ##### --------------------------
-
-#load saved data from step 01
-transition_data_scraped <- readRDS("processed_data/transition_data_scraped.rds")
-
-#some names have title after denoting they are the team lead
-#we'll capture that and remove title from name field
-agencyteams <- transition_data_scraped %>% 
-  mutate(
-    team_lead = if_else(str_detect(name, "Team Lead"), "Y", ""),
-    name = str_remove(name, ", Team Lead")
-  ) %>% 
-  select(agency, name, team_lead, everything())
-
-agencyteams
-
-#export for sharing
-agencyteams %>% 
-  select(-idstring) %>% 
-  write_xlsx("output/agencyreviewteams.xlsx")
+#### AGENCY TEAMS ##### --------------------------------------------------------
 
 
-#quick counts
-agencyteams %>% 
-  count(agency, sort = TRUE)
-
-agencyteams %>% 
-  count(most_recent_employment, sort = TRUE)
-
-agencyteams %>% 
-  count(source_of_funding, sort = TRUE)
-
-
-count_of_employment <- agencyteams %>% 
-  count(most_recent_employment, sort = TRUE)
-count_of_employment
-#note: the employment field likely needs to be standardized...Deliotte, etc
-
-agencyteams %>% 
-  filter(most_recent_employment == "Georgetown University") 
-
-
-
-### COMPARE agency teams with archived to find changes #### -------------------
+### COMPARE agency teams with previous version to find changes ######
 
 #load current data
 transition_data_current <- readRDS("processed_data/transition_data_scraped.rds")
@@ -56,10 +16,11 @@ transition_data_current
 transition_data_previous <- readRDS("archived_data/transition_data_archived_2020_11_24t09_52_05.rds")
 transition_data_previous
 
-#find new records added since previous
+#find new records of names added since previous
 anti_join(transition_data_current, transition_data_previous, by = "idstring")
 
-#compare departments by totals 
+
+#compare totals by department 
 agencycount_current <- transition_data_current %>% 
   count(agency, name = "current_count")
 
@@ -85,5 +46,34 @@ write_xlsx(agencycount_compare, "output/agencycount_compare.xlsx")
 
 
 
+#### Analysis ############
+
+#we'll create a newly named object to use from here on out 
+agencyteams <- transition_data_current
+
+#export for sharing
+agencyteams %>% 
+  select(-idstring) %>% 
+  write_xlsx("output/agencyreviewteams.xlsx")
+
+
+#quick counts
+agencyteams %>% 
+  count(agency, sort = TRUE)
+
+agencyteams %>% 
+  count(most_recent_employment, sort = TRUE)
+
+agencyteams %>% 
+  count(source_of_funding, sort = TRUE)
+
+
+count_of_employment <- agencyteams %>% 
+  count(most_recent_employment, sort = TRUE)
+count_of_employment
+#note: the employment field likely needs to be standardized...Deliotte, etc
+
+agencyteams %>% 
+  filter(most_recent_employment == "Georgetown University") 
 
 
